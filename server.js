1522f406f12b7578de58c4a4a032db683ea2ba34
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-04-17 10:53:58 +0200

    added multer for uploading formdata

--- server.js
+++ server.js
@@ -20,19 +20,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const upload = multer();
+
 app.use(cors());
 app.use(express.json());
 
 //boats route
 app.get("/api/v1/boats", getBoats);
 app.get("/api/v1/:boatId", getBoatDetail);
-app.post("/api/v1/boats", addBoat);
+app.post("/api/v1/boats", upload.none(), addBoat);
 app.delete("/api/v1/boats", deleteBoat);
 
 //resersation route
 app.get("/api/v1/reservation", getReservations);
 app.get("/api/v1/:reservationId", getReservationDetail);
-app.post("/api/v1/reservation", addReservation);
+app.post("/api/v1/reservation", upload.none(), addReservation);
 app.delete("/api/v1/reservation", deleteReservation);
 
 app.listen(PORT, () => console.log("Server listening on port", PORT));
